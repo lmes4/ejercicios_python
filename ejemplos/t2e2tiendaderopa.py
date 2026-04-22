e6d7349202ff65d6fe7f7721b38173d726fb5004
Lorena E <lorenasimarro4@gmail.com>
2026-04-22 19:28:34 +0200

    Update t2e2tiendaderopa.py

--- ejemplos/t2e2tiendaderopa.py
+++ ejemplos/t2e2tiendaderopa.py
@@ -1,18 +1,24 @@
-#Pedir al usuario que introduzca dos numeros
-#Convertimos a numero. int(sin decimales), float (puede tener decimales)
-numero1 = float(input("Introduzca el precio de la camiseta"))
-numero2 = float(input("Introduzca el precio de la sudadera"))
-numero3 = float(input("Introduzca el precio de la gorra"))
+#Define los siguientes precios
+camiseta = 10
+sudadera = 28.5
+gorra = 5.5
+
+cantidad_camisetas = int(input("Cuantas camisetas quieres"))
+cantidad_sudaderas = int(input("Cuantas sudaderas quieres"))
+cantidad_gorras = int(input("Cuantas gorras quieres"))
 
 #Hacemos las operaciones
+precio_total_camisetas = cantidad_camisetas * camiseta
+precio_total_sudaderas = cantidad_sudaderas * sudadera
+precio_total_gorras = cantidad_gorras * gorra
 #Calcula la suma total
-suma = numero1 + numero2 + numero3
-# Calcular IVA (21%)
-iva = suma * 0.21
-# Total con IVA
-total = suma + iva
+precio_total_compra = precio_total_camisetas + precio_total_sudaderas + precio_total_gorras
+#Calcular IVA (21%)
+iva = precio_total_compra * 0.21
+#Total con IVA
+total = precio_total_compra + iva
 
 #Imprimimos los resultados
-print("El total sin IVA es:", suma)
+print("El total sin IVA es:", precio_total_compra)
 print("El IVA (21%) es:", iva)
 print("El total con IVA incluido es:", total)
