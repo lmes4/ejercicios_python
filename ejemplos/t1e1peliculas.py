b80fdd5315f69846e34991e5e88ac54183f589bb
Lorena E <lorenasimarro4@gmail.com>
2026-04-24 17:58:19 +0200

    tema4

--- ejemplos/t1e1peliculas.py
+++ ejemplos/t1e1peliculas.py
@@ -1,30 +1,69 @@
 #Cadena
-nombre = "Pelicula favorita"
+nombre = "La boda de mi mejor amigo"
 #Numeros (int)
-year = 1997
-#Genero
-genero = "comedia romantica"
+año = 1997
 #Numeros (int)
-minutes = 104
+minutos = 104
 #Booleanos (True o False)
 tiene_premios = False
 
 #Imprimir las variables
 print("Nombre", nombre)
-print("Año", year)
-print("genero", genero)
-print("Minutos ", minutes)
+print("Año", año)
+print("Minutos ", minutos)
 print("Tiene premios?", tiene_premios)
 
+#Obtener valores introducidos por el usuario
+
+nombre_pelicula = input("Cual es tu pelicula favorita?")
+nombre_director = input("Cual es el director?")
+año_lanzamiento = input("En que año se estreno?")
+nombre_genero = input("Que genero tiene la pelicula?")
+minutos_duracion = input("Cuanto dura la pelicula?")
+# Booleano
+tiene_premios = input("¿Tiene premios? (si/no): ") == "no"
+
+
+print("Tu pelicula favorita es ", nombre_pelicula)
+
+print("El director de la pelicula es ", nombre_director)
+
+print("El año de lanzamiento fue ", año_lanzamiento)
+
+print("El genero de la pelicula es ", nombre_genero)
+
+print("La duracion de la pelicula es ", minutos_duracion)
+
+print("¿Tiene premios?", tiene_premios)
 
 #String
-name = "Favorite movie"
-#Gender
-gender = "Romantic comedy"
-#Booleans (True or False)
+nombre = "My Best Friend's Wedding"
+# Numbers (int)
+year = 1997
+# Numbers (int)
+minutes = 104
+# Boolean (True or False)
 has_awards = False
 
 #Print variables
-print("Name", name)
+print("Name:", nombre)
+print("Year:", year)
+print("Duration (minutes):", minutes)
+print("Has awards?", has_awards)
 
+#User input
+
+movie_name = input("What is your favorite movie? ")
+director_name = input("Who is the director? ")
+release_year = input("What year was it released? ")
+genre = input("What genre is the movie? ")
+duration = input("How long is the movie? ")
+#Boolean
+has_awards = input("Does it have awards? (yes/no): ") == "yes"
+
+print("Your favorite movie is:", movie_name)
+print("The director is:", director_name)
+print("Release year:", release_year)
+print("Genre:", genre)
+print("Duration:", duration)
 print("Has awards?", has_awards)
